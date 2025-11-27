f1cde11b52c465b2dbaf102abee3dc13cc0134e9
hersa rizky <hersa.syahputra@nextera.dev>
2025-11-27 11:09:24 +0700

    Fix QA-077: Redirect to admin/dashboard-list on first entry

--- dataviz-frontend/src/app/app.routes.ts
+++ dataviz-frontend/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { AuthGuard } from './core/auth/auth.guard';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/dashboard',
+    redirectTo: '/admin/dashboard-list',
     pathMatch: 'full'
   },
   {
@@ -43,6 +43,6 @@ export const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: '/dashboard'
+    redirectTo: '/admin/dashboard-list'
   }
 ]; 
