d11a949256b522b14f2fdf361d87452a3176af12
hersa rizky <hersa.syahputra@nextera.dev>
2025-12-01 13:30:38 +0700

    fix translation:fix key translation pop up export dashboard pdf in fr fix token expired:add check session when start add translation:global warning'section expiring soon'

--- dataviz-frontend/src/app/app.component.ts
+++ dataviz-frontend/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AuthService, User } from './core/auth/auth.service';
 import { FloatingChatComponent } from './shared/components/floating-chat/floating-chat.component';
+import { SessionMonitorService } from './core/auth/session-monitor.service';
 
 @Component({
   selector: 'app-root',
@@ -23,12 +24,13 @@ import { FloatingChatComponent } from './shared/components/floating-chat/floatin
 export class AppComponent implements OnInit {
   currentUser: User | null = null;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private sessionMonitor: SessionMonitorService) {}
 
   ngOnInit(): void {
     // Subscribe to authentication state changes
     this.authService.currentUser$.subscribe(user => {
       this.currentUser = user;
     });
+    this.sessionMonitor.isSessionValid();
   }
-} 
+}
